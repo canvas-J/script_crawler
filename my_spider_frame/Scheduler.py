9f1ba24e9a5bc5861639e3969408ad4480535197
canvas-J <1341281680@qq.com>
2018-05-13 21:50:02 +0800

    使符合PEP8

--- my_spider_frame/Scheduler.py
+++ my_spider_frame/Scheduler.py
@@ -12,7 +12,7 @@ class Spider_Scheduler(object):
         self.parser = HtmlParser()
         self.output = DataOutput()
 
-    def crawl(self,root_url):
+    def crawl(self, root_url):
         # 入口放url种子
         self.urlmanager.add_new_url(root_url)
         # 判断url管理器中是否有新的url，同时判断抓取了多少个url
@@ -23,12 +23,12 @@ class Spider_Scheduler(object):
                 # HTML下载器下载网页
                 html = self.downloader.download(new_url)
                 # HTML解析器抽取网页数据
-                new_urls, data = self.parser.parser(new_url,html)
+                new_urls, data = self.parser.parser(new_url, html)
                 # 将抽取到url添加到URL管理器中
                 self.urlmanager.add_new_urls(new_urls)
                 # 存储器将数据序列化
                 self.output.data_to_list(data)
-                print("已经抓取%s个链接"%self.urlmanager.old_url_size())
+                print("已经抓取%s个链接" % self.urlmanager.old_url_size())
             except Exception as e:
                 print("crawl failed")
         # 存储器输出成指定格式
